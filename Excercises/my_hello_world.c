b99162d18f4391b7cf9ee5784754ba600b7cb0a3
malharpandya <41825230+malharpandya@users.noreply.github.com>
2020-09-14 21:07:10 -0400

    Ex1
    Iconic hello world as an intro to C

---
+++ Excercises/my_hello_world.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello! My name is: Malhar Pandya\n\nI am a student in Computer Science at UTSC\n\nI love to play Badminton\n\nMy favourite colour is: Turquoise\n\nMy favourite series is: Brooklyn Nine-Nine\n");
+	return 0;
+}
